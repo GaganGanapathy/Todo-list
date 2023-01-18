29c45e321aefa3bd46281d764b0209a87a328515
GaganGanapathy <109824692+GaganGanapathy@users.noreply.github.com>
2023-01-19 00:38:25 +0530

    structural html

---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+const path = require('path');
+
+module.exports = {
+    entry: './src/index.js',
+    mode: 'development',
+    devtool: "inline-source-map",
+    devServer: {
+        static: './dist',
+    },
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(css)$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(scss)$/i,
+                use: [
+                  {
+                    loader: 'style-loader'
+                  },
+                  {
+                    loader: 'css-loader'
+                  },
+                  {
+                    loader: 'postcss-loader',
+                    options: {
+                      postcssOptions: {
+                        plugins: () => [
+                          require('autoprefixer')
+                        ]
+                      }
+                    }
+                  },
+                  {
+                    loader: 'sass-loader'
+                  }
+            
+                ],      
+            },
+        ],
+    }
+};
